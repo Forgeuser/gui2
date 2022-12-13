b95ef90b091fa286d9444d039defc7411820a806
Forgeuser <118595281+Forgeuser@users.noreply.github.com>
2022-12-13 17:16:14 +0200

    made the buttons have size and added colors center

--- main.java
+++ main.java
@@ -60,6 +60,36 @@ public class main  {
         p4.setBackground(Color.blue);
         p4.setPreferredSize(new Dimension(100,100));
 
+        JPanel p5 = new JPanel();
+        p5.setBackground(Color.GREEN);
+        p5.setPreferredSize(new Dimension(50,50));
+
+        p5.setLayout(new BorderLayout());
+
+        JPanel p6 = new JPanel();
+        p6.setBackground(Color.MAGENTA);
+        p6.setPreferredSize(new Dimension(50,50));
+
+        JPanel p7 = new JPanel();
+        p7.setBackground(Color.pink);
+        p7.setPreferredSize(new Dimension(50,50));
+
+        JPanel p8 = new JPanel();
+        p8.setBackground(Color.blue);
+
+        p8.setPreferredSize(new Dimension(50,50));
+        JPanel p9 = new JPanel();
+
+
+        p9.setBackground(Color.LIGHT_GRAY);
+        p9.setPreferredSize(new Dimension(50,50));
+
+        p5.add(p6,BorderLayout.SOUTH);
+        p5.add(p7,BorderLayout.WEST);
+        p5.add(p8,BorderLayout.EAST);
+        p5.add(p9,BorderLayout.NORTH);
+
+
 
 
 
@@ -75,12 +105,18 @@ ImageIcon e = new ImageIcon("hehe.png");
         f.add(p2,BorderLayout.EAST);
         f.add(p3,BorderLayout.SOUTH);
         f.add(p4,BorderLayout.WEST);
+        f.add(p5,BorderLayout.CENTER);
+
+
+        // Button's
+
+
         f.setTitle("border's  lol");
         f.setIconImage(e.getImage());
 
 
 
-        b.setBounds(0, 100, 50, 50);
+        b.setBounds(150, 150, 200, 200);
 
         b.setText("yo click me ");
         b.setFocusable(false);
